6d3b5ce8e615f99f26c86d48432324384437ff37
antfor <38752136+antfor@users.noreply.github.com>
2020-12-13 11:01:14 +0100

    ...

--- lab4/Expr.hs
+++ lab4/Expr.hs
@@ -1,13 +1,16 @@
 module Expr where
 
-import Parsing
+import           Parsing
+import           Data.Maybe
+import           Data.Char
+import QuickCheck
 
 --Part I
 x :: Expr
 x = Var 'x'
 
 num :: Double -> Expr
-num  = Num
+num = Num
 
 add :: Expr -> Expr -> Expr
 add = Binary Add
@@ -24,63 +27,55 @@ sin = Unary Sin
 data Expr = Num Double | Var Char | Unary UnOp Expr | Binary BinOp Expr Expr
 
 
-class Associativ a where
-    assoc :: a -> Assoc
-
-data Assoc = Left | Right | None
+data Assoc = ALeft | ARight | ANone
     deriving Eq
 
-class Associativ a where
+class Expression a where
     assoc :: a -> Assoc
-
-instance Associativ Expr where
-    assos e = case e of
-        Num _ -> None
-        Var _ -> None
-        Unary op _ -> pres op
-        Binary op _ -> pres op
-
--- https://rosettacode.org/wiki/Operator_precedence#Haskell
-class Presidens a where
     pres :: a -> Int
 
-instance Presidens Expr where
-    pres e = case e of
-        Num _ -> 9
-        Var _ -> 9
-        Unary op _ -> pres op
-        Binary op _ -> pres op
+-- https://rosettacode.org/wiki/Operator_precedence#Haskell
+instance Expression Expr where
+  assoc e = case e of
+    Num _         -> ANone
+    Var _         -> ANone
+    Unary op _    -> assoc op
+    Binary op _ _ -> assoc op
+  pres e = case e of
+    Num _         -> 9
+    Var _         -> 9
+    Unary op _    -> pres op
+    Binary op _ _ -> pres op
+
 
 data UnOp  = Sin | Cos
 
 instance UOP UnOp where
-    showU Sin = preFix "sin" ""  --B
-    showU Cos = preFix "cos " "" --B
-    evalU Sin = Prelude.sin      --C
-    evalU Cos = Prelude.cos      --C
+  showU Sin = preFix "sin" ""  --B
+  showU Cos = preFix "cos " "" --B
+  evalU Sin = Prelude.sin      --C
+  evalU Cos = Prelude.cos      --C
 
---    parU _ e@(Binary _ _ _) = addPar e --B
---    parU _ e = showExpr e              --B
-
-instance Presidens UnOp where
-    pres e = case e of
-        _ -> 9
+instance Expression UnOp where
+  assoc e = case e of
+    _ -> ARight
+  pres e = case e of
+    _ -> 9
 
 data BinOp = Mul | Add | Pow
 
 instance BOP BinOp where
-    showB Add = inFix " + " --B
-    showB Mul = inFix " * " --B
-    evalB Add = (+)         --C
-    evalB Mul = (*)         --C
+  showB Add = inFix " + " --B
+  showB Mul = inFix " * " --B
+  evalB Add = (+)         --C
+  evalB Mul = (*)         --C
 
---    parB Mul e@(Binary Add _ _) = addPar e --B
---    parB _ e = showExpr e                  --B
-
-instance Presidens UnOp where
-    pres e = case e of
-        Mul -> 7
-        Add -> 6
+instance Expression BinOp where
+  assoc e = case e of
+    _ -> ALeft
+  pres e = case e of
+    Mul -> 7
+    Add -> 6
 
 preFix :: String -> String -> String -> String
 preFix op s1 s2 = op ++ s1 ++ s2
@@ -93,36 +88,79 @@ postFix op s1 s2 = s1 ++ s2 ++ op
 class BOP a where
     showB :: a -> String -> String -> String
     evalB :: a -> Double -> Double -> Double
-    parB :: a -> Expr -> String
 
 class UOP a where
     showU :: a -> String -> String
     evalU :: a -> Double -> Double
-    parU :: a -> Expr -> String
+
+size :: Expr -> Int
+size Binary _ e1 e2 = 1 + size e1 + size e2
+size Unary _ e = 1 + size e
+size _ = 0
+
 
 --B-----------------------------
+instance Show Expr where
+  show = showExpr
 
 showExpr :: Expr -> String
-showExpr (Var c) = show c
-showExpr (Num n) = show n
-showExpr (Unary op e) = showU op (parU op e)
-showExpr (Binary op e1 e2) = showB op (parB op e1) (parB op e2)
+showExpr (Var c          ) = show c
+showExpr (Num n          ) = show n
+showExpr (Unary op e     ) = showU op (par op e)
+showExpr (Binary op e1 e2) = showB op (par op e1) (par op e2)
 
 par op e | pres op > pres e = inFix (showExpr e) "(" ")"
          | otherwise        = showExpr e
---addPar e = inFix (showExpr e) "(" ")"
+
 --C-----------------------------
 
 eval :: Expr -> Double -> Double
-eval (Var c) x = x
-eval (Num n) _ = n
-eval (Unary op e) n = evalU op (eval e n)
+eval (Var c          ) x = x
+eval (Num n          ) _ = n
+eval (Unary op e     ) n = evalU op (eval e n)
 eval (Binary op e1 e2) n = evalB op (eval e1 n) (eval e2 n)
 
 --D-----------------------------
 
 readExpr :: String -> Maybe Expr
+readExpr s = do
+  case parse expr s of
+    Nothing -> Nothing
+    Just e  -> if (null . snd) e then (Just . fst) e else Nothing
+
+expr, term, factor :: Parser Expr -- todo use assoc and pres
+expr = foldl1 add <$> chain term (char '+')
+term = foldl1 mul <$> chain factor (char '*')
+factor = Expr.sin <$> parsTrig "sin"
+      <|> Expr.cos <$> parsTrig "cos"
+      <|> Num <$> (readsP :: (Parser Double))
+      <|> char '(' *>  expr <*  char ')'
+      <|> Var 'x'<$  char 'x'
+ where
+     parsTrig trig = mapM_ char trig *> factor
 
 --E-----------------------------
+prop_ShowReadExpr :: Expr -> Bool
+prop_ShowReadExpr e = e == readExpr (show e)
+
+arbExpr :: Int -> Gen Expr
+arbExpr = undefined
+
+assocExpr :: Expr -> Expr --todo fix
+assocExpr (Binary Add (Binary Add e1 e2) e3) = assocExpr (Binary Add e1 (Binary Add e2 e3))
+assocExpr (Binary Mul (Binary Mul e1 e2) e3) = assocExpr (Binary Mul e1 (Binary Mul e2 e3))
+assocExpr (Binary op e1 e2) = Binary op (assocExpr e1) (assocExpr e2)
+assocExpr (Unary op e) = Unary op (assocExpr e)
+assocExpr e = e
+
+instance Arbitrary Expr where
+  arbitrary = sized arbExpr
 --F-----------------------------
+
+simplify :: Expr -> Expr
+simplify = undefined
+
 --G-----------------------------
+
+differentiate :: Expr -> Expr
+differentiate = undefined
