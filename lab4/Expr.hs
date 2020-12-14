02d01855234f7d300284cf386efb161d120059f5
antfor <38752136+antfor@users.noreply.github.com>
2020-12-14 18:24:15 +0100

    started on F

--- lab4/Expr.hs
+++ lab4/Expr.hs
@@ -144,7 +144,10 @@ factor = Expr.sin <$> parsTrig "sin"
 
 --E-----------------------------
 prop_ShowReadExpr :: Expr -> Bool
-prop_ShowReadExpr e = assocExpr e ==  (fromJust (readExpr (show e)))
+prop_ShowReadExpr e = assocExpr e ==  fromJust (readExpr (show e))
+--prop_ShowReadExpr e = (showread . showread) e ==  showread e
+ --where
+    --showread e = fromJust (readExpr (show e))
 
 arbExpr :: Int -> Gen Expr --todo
 arbExpr = rExpr
@@ -156,7 +159,7 @@ assocExpr (Binary op e1 e2) = Binary op (assocExpr e1) (assocExpr e2)
 assocExpr (Unary op e) = Unary op (assocExpr e)
 assocExpr e = e
 
-rExpr :: Int -> Gen Expr --todo
+rExpr :: Int -> Gen Expr --todo use size
 rExpr s = frequency [(1,rNum),(s,rBin s)]
     where
         range = 4 -- integer range
@@ -178,13 +181,65 @@ rExpr s = frequency [(1,rNum),(s,rBin s)]
 instance Arbitrary Expr where
   arbitrary = sized arbExpr
 --F-----------------------------
+-- 1 assocExpr
+-- 2 moveVar
+-- simplify'
 
 simplify :: Expr -> Expr
-simplify = undefined
+simplify = simplify' . moveVar . assocExpr
+ where
+    simplify' :: Expr -> Expr
+    simplify' (Unary op e) = Unary op (simplify' e)
+    simplify' (Binary op e1 e2) = binaryS op (simplify' e1) (simplify' e2)
+    simplify' e = e
 
-maybeEval :: Expr -> Maybe Expr 
+moveVar :: Expr -> Expr
+moveVar (Unary op e) = Unary op (moveVar e)
+moveVar (Binary op e1 e2) = binToList (Binary op (moveVar e1) (moveVar e2))
+moveVar e = e
+
+binToList :: Expr -> BinOp -> [Expr]
+binToList (Binary op1 e1 (Binary op2 e2 e3)) op0 | op0 == op1 && op1 == op2 = [e1, e2] ++ binToList e3
+binToList (Binary op1 e1 e2) op0 | op0 == op1 = [e1, e2]
+binToList e _ = [e]
+
+containVar :: Expr -> Bool
+containVar (Var _) = True
+containVar (Num _) = False
+containVar (Unary _ e) = containVar e
+containVar (Binary _ e1 e2) = containVar e1 || containVar e2
+
+-- "smart" constructors
+binaryS :: BinOp -> Expr -> Expr -> Expr
+binaryS Add = addS
+binaryS Mul = mulS
+
+addS :: Expr -> Expr -> Expr
+addS (Num 0) e = e
+addS e (Num 0) = e
+addS e1 e2 = Binary Add e1 e2
+
+mulS :: Expr -> Expr -> Expr
+mulS (Num 1) e = e
+mulS e (Num 1) = e
+mulS (Num 0) e = Num 0
+mulS e (Num 0) = Num 0
+mulS e1 e2 = Binary Mul e1 e2
 
 --G-----------------------------
 
-differentiate :: Expr -> Expr
-differentiate = undefined
+--differentiate :: Expr -> Expr
+--differentiate = simplify . diffExpr
+
+{-
+diffExpr :: Expr -> Expr
+diffExpr (Var _) = 1
+diffExpr (Num 0) = 0
+diffExpr (Unary op e     ) n = diffU op e
+diffExpr (Binary op e1 e2) n = diffB op e1 e2
+-}
+
+-- kedjereglen (sin och cos)
+-- Additionsregeln (add)
+-- Produktregeln (mul)
+-- var = x
